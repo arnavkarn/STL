78a8ecd383357e0f2676592e1c750d7cb7b91bcf
Arnav Karn <42885032+arnavkarn@users.noreply.github.com>
2020-12-16 23:56:23 +0545

    Update set.cpp

--- set.cpp
+++ set.cpp
@@ -30,7 +30,9 @@ int main()
     auto it1=s.find(6); //as 6 is not present it will point to the address after the last element
     s.erase(4); //erases 4
     s.erase(it); //erases 5
-    s.erase(s.begin(),s.end()-1); //deletes all except last element
+    auto a=s.find(1);
+    auto b=s.find(3);
+    s.erase(a,b); //deletes 1 and 2
     auto it2=s.lower_bound(3); //returns address of 3
     auto it3=s.upper_bound(3); //as it should point to greater than 3 which does not exist thus points to address after last element
     auto it4=s.lower_bound(4); //as it is not present points to address after last element
