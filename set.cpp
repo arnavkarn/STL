61cd6231d33c1776a97a2a27661209b7a815963d
Arnav Karn <42885032+arnavkarn@users.noreply.github.com>
2020-12-16 23:52:10 +0545

    Update set.cpp

--- set.cpp
+++ set.cpp
@@ -26,4 +26,14 @@ int main()
     s.insert(5);
     printSet(s);
     cout<<"Size is : "<<s.size()<<endl;
+    auto it=s.find(5); //points to position of 5 in p
+    auto it1=s.find(6); //as 6 is not present it will point to the address after the last element
+    s.erase(4); //erases 4
+    s.erase(it); //erases 5
+    s.erase(s.begin(),s.end()-1); //deletes all except last element
+    auto it2=s.lower_bound(3); //returns address of 3
+    auto it3=s.upper_bound(3); //as it should point to greater than 3 which does not exist thus points to address after last element
+    auto it4=s.lower_bound(4); //as it is not present points to address after last element
+    auto it5=s.upper_bound(4); //as it should point to greater than 4 which does not exist thus points to address after last element
+    return 0;
 }
